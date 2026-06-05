446ad7ce7c06f9344a38a087f6e2e4d7698d5081
Konstantin Smirnov <ks@privatepractice.app>
2026-06-05 15:28:23 +0300

    Update update-flatpak-sources.sh

--- scripts/update-flatpak-sources.sh
+++ scripts/update-flatpak-sources.sh
@@ -6,9 +6,3 @@ set -eu
   -PmeshappProtocClassifier=linux-x86_64 \
   -PflatpakSourcesArch=x86_64 \
   "$@"
-
-./gradlew flatpakGradleGenerator --no-configuration-cache \
-  -PmeshappJavaFxPlatform=linux-aarch64 \
-  -PmeshappProtocClassifier=linux-aarch_64 \
-  -PflatpakSourcesArch=aarch64 \
-  "$@"
